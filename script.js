0dd63a62b99ce0246ecb59d7091e8b185aee3768
Sarthak <sarthakrautelanew@gmail.com>
2022-07-01 02:09:53 +0530

    added a new ui for the game , the idea is to display images of rock paper scissor on the topfrom both player and cpu side,meanwhile the score is diplayed below. more styling is needed and need  to add some js for the images.

--- script.js
+++ script.js
@@ -1,82 +1,63 @@
+let playerChoice =0;
+let cpuChoice =0;
+let cWin=0;
+let pWin=0;
+const btn = document.querySelectorAll('button');
+const ps = document.querySelector('.ps');
+const cs = document.querySelector('.cs');
 
-let pWin =0;
-let cWin =0;
+
+const playerClass =['r','p','s'];
 
 function cpuReturn(){
-    let choice = Math.floor(Math.random()*3)+1;
-return choice;
-}
-//janken is rock paper scissor in japanaese
-function toJanken( num){
-    let play;
-    if( num==1)
-    play="rock" ;
-    else if(num==2)
-    play="paper";
-    else 
-    play="scissor";
-return play;
+    let choice = Math.floor(Math.random()*3)+1;  
+ cpuChoice =choice;
 }
 
-// function toNmber(string){
-//     let  num;
-//     if(string=='rock')
-//       num=1;
-//     if(string=='paper')
-//     num=2;
-//     if(string=='scisssor')
-//     num=3; 
-//     return num;
-// }
+// function to check who is the winner
+function checkWin(){
+    if ( playerChoice==1 && cpuChoice==2)
+    cWin++;
 
-//this function updates the player score
-function checkWin( player,Cpu){
+    else if ( playerChoice==2 && cpuChoice==1)
+    pWin++;
 
-    if ( player=="rock" && Cpu=="paper")
-     cWin++;
+    else if ( playerChoice==2 && cpuChoice==3)
+    cWin++;
 
-     else if ( player=="paper" && Cpu=="rock")
+    else if ( playerChoice==1 && cpuChoice==3)
+    pWin++;
+
+    else if ( playerChoice==3 && cpuChoice==2)
      pWin++;
 
-     else if ( player=="paper" && Cpu=="scissor")
-     cWin++;
-
-     else if ( player=="rock" && Cpu=="scissor")
-     pWin++;
-
-    else if ( player=="scissor" && Cpu=="paper")
-      pWin++;
-
-    else if ( player=="scissor" && Cpu=="rock")
-     cWin++;
-      
-}
-
-
- 
-//updates score on the screen
-function update(){
-    alert("current score is :\nHuman: "+pWin +"    ||   computer : "+ cWin );
-}
-//main code that runs all the othe r  function together
-while(pWin!=3 && cWin !=3)
-{
-
-    let input = prompt("enter , rock ,paper or scissor");
-    input=input.trim();
-    input=input.toLowerCase();
-    let cInput = toJanken(cpuReturn());
-    alert("cpu choose "+ cInput);
-checkWin(input,cInput);
-update();
-if(pWin==3)
-alert("player wins the game!!!!!");
-else if(cWin==3)
-{
-    alert("cpu wins the game !!!!!");
+    else if ( playerChoice==3 && cpuChoice==1)
+    cWin++;
 }
 
 
 
+const out = document.querySelector('.score');
 
-}
+
+btn.forEach((player)=>{
+    player.addEventListener('click' , function(e){
+        for(let i=0;i<3;i++){
+        if(player.classList.contains(playerClass[i]))
+    {    playerChoice=i+1;
+        // console.log(playerChoice);
+       
+        cpuReturn();
+        checkWin();
+        console.log( "cpu :"+ cWin );
+        console.log( "player"+ pWin);
+        ps.textContent=`                      player score :  ${pWin}      `;
+        cs.textContent=`                      computer score :  ${cWin}      `;
+
+    }
+    }
+    })
+    
+});
+
+
