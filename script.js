d99be1016cd809c206749894ebfbf147668131cf
Sarthak <sarthakrautelanew@gmail.com>
2022-07-02 04:43:25 +0530

    replaced the layout so that options are at the bottom . added images to display both cpu and player choices .

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ let playerChoice =0;
 let cpuChoice =0;
 let cWin=0;
 let pWin=0;
+const pPhoto =["rock.jpeg","paper.jpg","scissor_left.jpeg"];
+const cPhoto =["rock.jpeg","paper.jpg","scissor_right.jpeg"];
+
 const btn = document.querySelectorAll('button');
 const ps = document.querySelector('.ps');
 const cs = document.querySelector('.cs');
-
+const dis_play =document.querySelector('.player');
+const dis_cpu =document.querySelector('.cpu');
 
 const playerClass =['r','p','s'];
 
 function cpuReturn(){
-    let choice = Math.floor(Math.random()*3)+1;  
+    
+    let choice = -1;
+    choice = Math.floor(Math.random()*3)+1;  
  cpuChoice =choice;
 }
 
@@ -46,13 +52,16 @@ btn.forEach((player)=>{
         if(player.classList.contains(playerClass[i]))
     {    playerChoice=i+1;
         // console.log(playerChoice);
-       
+       do{
         cpuReturn();
+       }while(playerChoice==cpuChoice)
         checkWin();
-        console.log( "cpu :"+ cWin );
-        console.log( "player"+ pWin);
+        console.log( "cpu :"+ cpuChoice );
+        console.log( "player"+ playerChoice);
         ps.textContent=`                      player score :  ${pWin}      `;
         cs.textContent=`                      computer score :  ${cWin}      `;
+        dis_play.innerHTML=`<img src="images/${pPhoto[i]}" style="width: 100%; height: 100%;" >` ;
+        dis_cpu.innerHTML=`<img src="images/${cPhoto[cpuChoice-1]}" style="width: 100%; height: 100%;" >` ;
 
     }
     }
@@ -60,4 +69,6 @@ btn.forEach((player)=>{
     
 });
 
+cs.style.cssText = 'font-size : 80px ; color: blue; text-align :center';
+ps.style.cssText = 'font-size : 80px ; color: blue; text-align :center';
 
